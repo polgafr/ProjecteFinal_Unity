d77cd63b537d580b42cb38fa19b115f9a8cebe49
polgafr <polgafr@gmail.com>
2017-06-02 00:01:57 +0200

    Noves scenes

---
+++ Assets/Prefabs/Intro.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+	
+	public GameObject intro;
+
+	// Use this for initialization
+	void Start () {
+		mostrarIntro ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void OK(){		
+		Time.timeScale = 1;
+		intro.SetActive(false);
+        Debug.Log("Hola");
+
+	}
+
+	public void mostrarIntro(){
+		Time.timeScale = 0;
+		intro.SetActive(true);
+	}
+}
---
+++ Assets/Prefabs/MenuPausa.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    bool isPause = false;
+    public GameObject menu;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPause = !isPause;
+            if (isPause)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+        }
+    }
+
+    
+
+    void OnGUI()
+    {
+        if (isPause)
+        {
+            menu.SetActive(true);
+        }
+        else
+        {
+            menu.SetActive(false);
+        }
+
+    }
+
+	public void resume(){
+		isPause = false;
+        Time.timeScale = 1;
+	}
+
+	public void irMenu(){
+		SceneManager.LoadScene(1);
+	}
+
+    public void restart()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+}
